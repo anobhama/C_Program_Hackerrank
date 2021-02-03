fd2ceeb3c0ef3c9abac1526c84a6594439396fdb
Anobhama V <anobhama99@gmail.com>
2021-02-03 19:08:19 +0530

    Pointers in C

---
+++ pointers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void update(int *a,int *b) {
+  int a1,b1;
+  a1= *a + *b;
+  b1= *a - *b;   
+  *a = a1;
+  *b = abs(b1);
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
